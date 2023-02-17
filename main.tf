01c3d388724fa9a169b678ec10a8ca67f8801d0a
Demola Onifade <donifade3484@mail.dbu.edu>
2023-02-17 01:37:25 -0600

    set up provisoners

--- main.tf
+++ main.tf
@@ -105,7 +105,29 @@ resource "aws_instance" "myapp-server" {
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
-    user_data = file("entry-script.sh")
+    #user_data = file("entry-script.sh")
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_location)
+    }
+
+    provisioner "file" {
+        source = "entry-script.sh"
+        destination = "/home/ec2-user/entry-script-on-ec2.sh"
+    }
+
+    provisioner "remote-exec" {
+        script = file("entry-script.sh")
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${self.public_ip} > output.txt"
+    }
+
+
 
     tags = {
         Name = "${var.env_prefix}-server"
